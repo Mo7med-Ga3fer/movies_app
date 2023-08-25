cd293985524be99cc3730219f43d708b2b81a2ca
Mo7medGa3fer <Mo7medGa3fer@gmail.com>
2023-08-25 07:58:07 +0300

    make home more responsive

--- lib/features/home/presentation/views/widgets/now_showing_item.dart
+++ lib/features/home/presentation/views/widgets/now_showing_item.dart
@@ -9,19 +9,23 @@ class NowShowingItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 9),
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width * 0.43,
+      child: AspectRatio(
+        aspectRatio: 2/4,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(
-              height: MediaQuery.of(context).size.height * 0.3,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  image: const DecorationImage(
-                    image: NetworkImage('https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_FMjpg_UX1000_.jpg'),
-                    fit: BoxFit.fill,
-                    ),
+              height: MediaQuery.of(context).size.height * 0.29,
+              child: AspectRatio(
+                aspectRatio: 2.8/4,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    image: const DecorationImage(
+                      image: NetworkImage('https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_FMjpg_UX1000_.jpg'),
+                      fit: BoxFit.fill,
+                      ),
+                  ),
                 ),
               ),
             ),
--- lib/features/home/presentation/views/widgets/popular_movies_list_item.dart
+++ lib/features/home/presentation/views/widgets/popular_movies_list_item.dart
@@ -10,7 +10,7 @@ class PopularMoviesListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 125,
+      height: MediaQuery.of(context).size.height * 0.15,
       child: Row(
         children: [
           AspectRatio(
